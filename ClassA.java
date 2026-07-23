64fdc6ff6c70ec59f755be177df08ab30e9e65ab
CalvinLin5336 <alfa5336@gmail.com>
2026-07-23 14:17:18 +0800

    feat(A): Add ClassA with name and version fields

---
+++ ClassA.java
@@ -0,0 +1,15 @@
+```java
+public class ClassA {
+    private String name;
+    private int version;
+
+    public ClassA(String name, int version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    public String getInfo() {
+        return name +  - v + version;
+    }
+}
+```
