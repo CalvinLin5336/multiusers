4168b1a870216337227dd002d2840c825d441d49
CalvinLin5336 <alfa5336@gmail.com>
2026-07-23 15:27:50 +0800

    no message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
-```java
+java
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello from C!");  // C 的修改
     }
 }
-```
